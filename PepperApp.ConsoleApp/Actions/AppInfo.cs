3f565e4f1f9d181a53ff2044ff60109a26cc0b87
Nick Hill <nicholaspatrickhill@gmail.com>
2023-03-26 18:36:22 -0400

    fixed typo in app info

--- PepperApp.ConsoleApp/Actions/AppInfo.cs
+++ PepperApp.ConsoleApp/Actions/AppInfo.cs
@@ -14,7 +14,7 @@ PepperApp was built using Visual Studio and .NET 7.0.
 PepperApp helps users manage information about hot peppers and their Scoville Heat Unit ratings.
 The Scoville scale is a tool for measuring the spiciness or pungency of hot peppers.
 The scale measures the amount of capsaicin in a pepper and assigns it a number rating in Scoville Heat Units (SHUs).
-Hot pepper enthusiasts and hot sauce artisans may find PepperApp useful for storing and updating information about their favorite peppers or for discovering new varities to try in their recipes.
+Hot pepper enthusiasts and hot sauce artisans may find PepperApp useful for storing and updating information about their favorite peppers or for discovering new varieties to try in their recipes.
 
 The following items from the Feature List are implemented:
 - Create 3 or more unit tests for your application.
